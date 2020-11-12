1e7837d4457d351b1ba986a3059bbd10a8a88231
narellakloster <narellakloster79@gmail.com>
2020-11-12 16:02:54 -0300

    agrego clases

---
+++ src/main/java/logica/RenglonPuntuacion.java
@@ -0,0 +1,11 @@
+package logica;
+
+public class RenglonPuntuacion extends RenglonGeneral {
+	
+	private Integer idRenglonPuntuacion;
+	private Integer puntos;
+	private Integer tantosAfavor;
+	private Integer tantosEnContra;
+	private Integer diferenciaDeTantos;
+
+}
---
+++ src/main/java/logica/TablaDePosiciones.java
@@ -0,0 +1,11 @@
+package logica;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TablaDePosiciones {
+
+	private Integer idTablaDePosiciones;
+	private Set renglonGeneral = new HashSet(0);
+	private Fixture fixture; 
+}
